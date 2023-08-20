2617f886759574db62717527ccb59849469c4ed7
Santiago <santiago-aviles@hotmail.com>
2023-08-20 18:03:42 -0500

    Modificación de los mensajes de vidas y resultados

--- PyLab01/src/Principal.java
+++ PyLab01/src/Principal.java
@@ -9,15 +9,15 @@ public class Principal {
         Scanner sc = new Scanner(System.in);
         do {
             vida++;
-            System.out.println("Vida:" + vida + "\t¿Cual es el numero secreto?");
+            System.out.println("Numero de vidas:" + vida + "\t¿Cual es el numero secreto?");
             rp = sc.nextInt();
             if (rp == 1234)
                 fl = true;
 
         } while (vida < 3 && !fl);
         if (fl == true)
-            System.out.println("Adivinaste!!!!!!");
+            System.out.println("Has adivinado!");
         else
-            System.out.println("ups, Perdiste");
+            System.out.println("Has perdido...");
     }
 }
